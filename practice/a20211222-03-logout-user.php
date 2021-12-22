e897ebaa6a345da114f79f794f81e96498562650
Shinder Lin <lsd0125@gmail.com>
2021-12-22 11:50:28 +0800

    多帳號登入

--- practice/a20211222-03-logout-user.php
+++ practice/a20211222-03-logout-user.php
@@ -2,6 +2,8 @@
 session_start();
 
 unset($_SESSION['user']);
+unset($_SESSION['user2']);
 
-header("Location: a20211222-02-login-form.php");
+// header("Location: a20211222-02-login-form.php");
+header("Location: ". $_SERVER['HTTP_REFERER']);
 
