74715f377b0a0ac04d222ba5aaebf40972e4026e
Shinder Lin <lsd0125@gmail.com>
2021-12-22 14:09:25 +0800

    PDO 連線 2

--- practice/__connect_db.php
+++ practice/__connect_db.php
@@ -7,5 +7,16 @@ $db_pass = 'admin';
 
 $dsn = "mysql:host={$db_host};dbname={$db_name};charset=utf8";
 
-$pdo = new PDO($dsn, $db_user, $db_pass);
+$db_options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+];
+
+try{
+    $pdo = new PDO($dsn, $db_user, $db_pass, $db_options);
+} catch (PDOException $ex){
+    echo '**** *****'. $ex->getMessage();
+}
+
 
