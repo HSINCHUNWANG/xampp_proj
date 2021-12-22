08ae58b6d7ce351ff91ec85dc47c6e7b0f7e54b8
Shinder Lin <lsd0125@gmail.com>
2021-12-22 12:05:13 +0800

    HTTP_REFERER

--- practice/a20211222-03-logout-user.php
+++ practice/a20211222-03-logout-user.php
@@ -5,5 +5,5 @@ unset($_SESSION['user']);
 unset($_SESSION['user2']);
 
 // header("Location: a20211222-02-login-form.php");
-header("Location: ". $_SERVER['HTTP_REFERER']);
+header("Location: ". ($_SERVER['HTTP_REFERER'] ?? 'a20211222-04-login-form.php'));
 
