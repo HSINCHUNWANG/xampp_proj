19af366bc15d6ba21af23bc732a30c0d89890284
Shinder Lin <lsd0125@gmail.com>
2021-12-22 14:55:06 +0800

    PDO 讀取多筆

--- practice/a20211222-05-db-query.php
+++ practice/a20211222-05-db-query.php
@@ -2,10 +2,17 @@
 
 require __DIR__. '/__connect_db.php';
 
-$sql = "SELECT * FROM address_book";
+$sql = "SELECT * FROM address_book LIMIT 10";
 
 $stmt = $pdo->query($sql);
 
-$row = $stmt->fetch();
+// $row = $stmt->fetch();
+// $row = $stmt->fetchAll(); // 所有資料筆數
 
-echo json_encode($row);
+while($row = $stmt->fetch()){
+    echo "{$row['name']} <br>";
+}
+
+
+
+echo json_encode($row);  // 最後拿到的值是 false
