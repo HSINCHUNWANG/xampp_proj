2bf13af0aead08442c211baff20d0aa90439ec33
Shinder Lin <lsd0125@gmail.com>
2021-12-24 15:08:19 +0800

    filter_var()

--- address-book/insert-api.php
+++ address-book/insert-api.php
@@ -7,18 +7,35 @@ $output = [
     'error' => '',
 ];
 
-// TODO: 檢查欄位資料
+$name = $_POST['name'] ?? '';
+$email = $_POST['email'] ?? '';
 
-// 錯誤的作法
-$sql = sprintf("INSERT INTO `address_book`(
+
+// TODO: 檢查欄位資料
+if(empty($name)) {
+    $output['code'] = 401;
+    $output['error'] = '請輸入正確的姓名';
+    echo json_encode($output); exit;
+}
+if(empty($email) or !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $output['code'] = 405;
+    $output['error'] = '請輸入正確的email';
+    echo json_encode($output); exit;
+}
+
+
+
+
+$sql = "INSERT INTO `address_book`(
                            `name`, `email`, `mobile`, `birthday`, `address`, `created_at`
-                           ) VALUES ('%s', '%s', '%s', '%s', '%s', NOW() )",
+                           ) VALUES (?, ?, ?, ?, ?, NOW() )";
+/*
     $_POST['name'] ?? '',
     $_POST['email'] ?? '',
     $_POST['mobile'] ?? '',
     $_POST['birthday'] ?? '',
     $_POST['address'] ?? ''
-);
+*/
 // echo $sql; exit;  // 除錯
 
 $stmt = $pdo->query($sql);
