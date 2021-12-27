c2391b20daaf0c0606a3bd275dc7baacb7a2c6f8
Shinder Lin <lsd0125@gmail.com>
2021-12-27 09:42:13 +0800

    json unescape unicode

--- address-book/insert-api.php
+++ address-book/insert-api.php
@@ -16,17 +16,17 @@ $mobile = $_POST['mobile'] ?? '';
 if(empty($name)) {
     $output['code'] = 401;
     $output['error'] = '請輸入正確的姓名';
-    echo json_encode($output); exit;
+    echo json_encode($output, JSON_UNESCAPED_UNICODE); exit;
 }
 if(empty($email) or !filter_var($email, FILTER_VALIDATE_EMAIL)) {
     $output['code'] = 405;
     $output['error'] = '請輸入正確的email';
-    echo json_encode($output); exit;
+    echo json_encode($output, JSON_UNESCAPED_UNICODE); exit;
 }
 if(empty($mobile) or !preg_match("/^09\d{2}-?\d{3}-?\d{3}$/", $mobile)) {
     $output['code'] = 407;
     $output['error'] = '請輸入正確的手機號碼';
-    echo json_encode($output); exit;
+    echo json_encode($output, JSON_UNESCAPED_UNICODE); exit;
 }
 
 
@@ -52,7 +52,7 @@ $output['rowCount'] = $stmt->rowCount();
 
 
 
-echo json_encode($output);
+echo json_encode($output, JSON_UNESCAPED_UNICODE);
 
 
 
